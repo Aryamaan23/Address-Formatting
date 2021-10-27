e67b42e67a3eaeedc2af9ab5b8057c11e41d6405
Aryamaan <pandeyaryamaan@gmail.com>
2021-10-28 02:43:27 +0530

    feat: add language

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel
 from urllib.request import urlopen
 import json
 import FCMManager as fcm
+from googletrans import Translator
 from indic_transliteration import sanscript
 from indic_transliteration.sanscript import transliterate
 app = FastAPI()
@@ -14,6 +15,7 @@ geolocator = Nominatim(user_agent="geoapiExercises")
 
 key = 'd908ec2a841244e0892ac9b12db8a453'
 geocoder = OpenCageGeocode(key)
+translator = Translator()
 #geo_url = 'http://maps.googleapis.com/maps/api/geocode/json'
 #my_address = {'address': '21 Ramkrishana Road, Burdwan, East Burdwan, West Bengal, India', 
              #'language': 'en'}
@@ -70,3 +72,81 @@ async def addressformatter(address: str):
     
     #return {"address": address, "latitude": results[0]['geometry']['lat'], "longitude":results[0]['geometry']['lng'],"Country Code":results[0]['components']['country_code']}
 
+
+
+@app.post("/addressformatterregionallanguages/")
+async def addressformatter(address: str , languageoption: str):
+    """
+    from_lang=''
+    to_lang=''
+    address2=""
+    address2= translator.translate({address},src= "gu",dest= "en")
+
+   
+    print(address2)
+    """
+
+
+    
+
+    results = geocoder.geocode(address)
+    latitude=results[0]['geometry']['lat']
+    longitude=results[0]['geometry']['lng']
+    #print(u'%f;%f;%s;%s' % (results[0]['geometry']['lat'], 
+                        #results[0]['geometry']['lng'],
+                        #results[0]['components']['country_code']))
+    
+    location = geolocator.reverse(str(latitude)+","+str(longitude))
+    print(location)
+    address2 = location.raw['address']
+    print(address2)
+    
+    #try:
+    results = geocoder.reverse_geocode(latitude, longitude, language='en', no_annotations='1')
+    print(results)
+    x=transliterate(results[0]['formatted'], sanscript.ITRANS, sanscript.DEVANAGARI)
+    y=transliterate(address2['amenity'], sanscript.ITRANS, sanscript.DEVANAGARI)
+    z=transliterate(address2['road'], sanscript.ITRANS, sanscript.DEVANAGARI)
+    a=transliterate(address2['neighbourhood'], sanscript.ITRANS, sanscript.DEVANAGARI)
+    b=transliterate(address2['suburb'], sanscript.ITRANS, sanscript.DEVANAGARI)
+    c=transliterate(address2['city_district'], sanscript.ITRANS, sanscript.DEVANAGARI)
+    d=transliterate(address2['city'], sanscript.ITRANS, sanscript.DEVANAGARI)
+    e=transliterate(address2['county'], sanscript.ITRANS, sanscript.DEVANAGARI)
+    f=transliterate(address2['state_district'], sanscript.ITRANS, sanscript.DEVANAGARI)
+    g=transliterate(address2['state'], sanscript.ITRANS, sanscript.DEVANAGARI)
+    h=transliterate(address2['postcode'], sanscript.ITRANS, sanscript.DEVANAGARI)
+    i=transliterate(address2['country'], sanscript.ITRANS, sanscript.DEVANAGARI)
+    j=transliterate(address2['country_code'], sanscript.ITRANS, sanscript.DEVANAGARI)
+
+    am= transliterate("amenity",sanscript.ITRANS, sanscript.DEVANAGARI)
+    am2=transliterate("road",sanscript.ITRANS, sanscript.DEVANAGARI)
+    am3=transliterate("neighbourhood",sanscript.ITRANS, sanscript.DEVANAGARI)
+    am4=transliterate("suburb",sanscript.ITRANS, sanscript.DEVANAGARI)
+    am5=transliterate("city_district",sanscript.ITRANS, sanscript.DEVANAGARI)
+    am6=transliterate("city",sanscript.ITRANS, sanscript.DEVANAGARI)
+    am7=transliterate("county",sanscript.ITRANS, sanscript.DEVANAGARI)
+    am8=transliterate("state_district",sanscript.ITRANS, sanscript.DEVANAGARI)
+    am9=transliterate("state",sanscript.ITRANS, sanscript.DEVANAGARI)
+    am10=transliterate("postcode", sanscript.ITRANS, sanscript.DEVANAGARI)
+    am11=transliterate("country",sanscript.ITRANS, sanscript.DEVANAGARI)
+    am12=transliterate("country_code",sanscript.ITRANS, sanscript.DEVANAGARI)
+    final_updated={"address": {
+            am: y,
+            am2: z,
+            am3: a,
+            am4: b,
+            am5: c,
+            am6: d,
+            am7: e,
+            am8: f,
+            am9: g,
+            am10: h,
+            am11: i,
+            am12: j
+
+        }
+    }
+    if results and len(results):
+        return {"Formatted address": x,"address": final_updated}
+    #except RateLimitExceededError as ex:
+          #print(ex)
